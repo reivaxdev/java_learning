10a3c9f14be1b21c4fca0e12bca87e69185269a0
dawjsierra <148371696+reivaxdev@users.noreply.github.com>
2023-11-17 01:14:05 +0100

    subida 17/11
    Variables, comentarios, primera clase...

---
+++ PrimerosPasos/src/Variables.java
@@ -0,0 +1,18 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		
+		int edad;
+		
+		edad = 35;
+		
+		System.out.println(edad);
+		
+		edad = 76;
+		
+		System.out.println(edad);
+
+	}
+
+}
